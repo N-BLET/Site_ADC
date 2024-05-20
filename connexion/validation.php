34758b080e5dcf4cb871cece23feda9221df37ac
Nicolas BLET <nicolas.blet@hotmail.com>
2024-05-20 23:10:40 +0200

    Modification du fichier validation.php

--- connexion/validation.php
+++ connexion/validation.php
@@ -6,7 +6,6 @@ if (isset($_GET["jeton"])) {
 
 	// On recherche le client lié à ce jeton
 	$client = ClientRepo::getClientSelonJeton($jeton);
-	var_dump($client);
 	if ($client != null) {
 		// Si le client n'est pas valide, on le valide
 		if (!$client->getEstValide()) {
